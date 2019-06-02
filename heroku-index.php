34c0a8e6417b0a14961a4947c5b47517ee15a1a9
supersmitty2018 <48794241+supersmitty2018@users.noreply.github.com>
2019-06-02 21:32:53 +0200

    added database access4

--- heroku-index.php
+++ heroku-index.php
@@ -1,13 +1,46 @@
 <?php
+
+require('../vendor/autoload.php');
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Register view rendering
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+// Our web handlers
+
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('index.twig');
+});
+
+$app->get('/cowsay', function() use($app) {
+  $app['monolog']->addDebug('cowsay');
+  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+});
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return str_repeat('Hello', getenv('TIMES'));
+});
+
+$dbopts = parse_url(getenv('DATABASE_URL'));
 $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
                array(
                 'pdo.server' => array(
                    'driver'   => 'pgsql',
-                   'user' => "xmmutkkqfftoxs",
-                   'password' => "66872be8b5b4f4677cb34f033b54c18f7b8205e1a133eba23adeb99d882e40ab",
-                   'host' => "ec2-54-221-214-3.compute-1.amazonaws.com",
-                   'port' =>  "5432",
-                   'dbname' => "d9f424d1vt6cmt")
+                   'user' => $dbopts["user"],
+                   'password' => $dbopts["pass"],
+                   'host' => $dbopts["host"],
+                   'port' => $dbopts["port"],
+                   'dbname' => ltrim($dbopts["path"],'/')
                    )
                )
 );
@@ -26,4 +59,5 @@ $app->get('/db/', function() use($app) {
     'names' => $names
   ));
 });
-?>
+
+$app->run();
