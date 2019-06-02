c70cfe61006695f48d64b5998672afa8004d6af6
supersmitty2018 <48794241+supersmitty2018@users.noreply.github.com>
2019-06-02 23:12:18 +0200

    Connected and queried the database

--- heroku-index.php
+++ heroku-index.php
@@ -10,23 +10,25 @@ try{
  $conn = new PDO($dsn);
  
 
+
+
+ // display a message if connected to the PostgreSQL successfully
+ if($conn){
+ echo "Connected to the <strong>$db</strong> database successfully!";
+ }
+
  //Make query to test connection
  $query = "SELECT * FROM users";
  $result = pg_query($query);
 
  $mydata = pg_fetch_assoc($result); 
- echo $mydata[username];
+ echo ($mydata->username);
 
  if (isset($result)) {
      echo("result is set </br>");
  } else {
      echo ('result is not set</br>');
     }
-
- // display a message if connected to the PostgreSQL successfully
- if($conn){
- echo "Connected to the <strong>$db</strong> database successfully!";
- }
 
 }catch (PDOException $e){
  // report error message
