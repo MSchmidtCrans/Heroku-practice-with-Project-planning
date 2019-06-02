340483372dbebda5119b391303778b2161cf40e1
supersmitty2018 <48794241+supersmitty2018@users.noreply.github.com>
2019-06-02 22:39:52 +0200

    adapt php script to accept psql connection2

--- heroku-index.php
+++ heroku-index.php
@@ -2,7 +2,7 @@
 
 echo ('Hallo Michael');
 
-
+require_once 'dbconfig.php';
 
 $dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
  
@@ -10,10 +10,12 @@ try{
  // create a PostgreSQL database connection
  $conn = new PDO($dsn);
  
+ 
  // display a message if connected to the PostgreSQL successfully
  if($conn){
  echo "Connected to the <strong>$db</strong> database successfully!";
  }
+ 
 }catch (PDOException $e){
  // report error message
  echo $e->getMessage();
