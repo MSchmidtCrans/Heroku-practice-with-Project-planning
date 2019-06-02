d3c131877c62c67ebbcad63c117c151aaa3edf6b
supersmitty2018 <48794241+supersmitty2018@users.noreply.github.com>
2019-06-02 21:50:57 +0200

    added new database connection function

--- heroku-index.php
+++ heroku-index.php
@@ -1,7 +1,10 @@
 <?php
 echo ('Hallo Michael');
 
-$conn_string = "host=ec2-54-221-214-3.compute-1.amazonaws.com port=5432 dbname=d9f424d1vt6cmt user=laxmmutkkqfftoxsmb password=66872be8b5b4f4677cb34f033b54c18f7b8205e1a133eba23adeb99d882e40ab";
-$dbconn4 = pg_connect($conn_string);
+function getdb() {
+    $db = pg_connect("host=ec2-54-221-214-3.compute-1.amazonaws.com port=5432 dbname=d9f424d1vt6cmt user=laxmmutkkqfftoxsmb password=66872be8b5b4f4677cb34f033b54c18f7b8205e1a133eba23adeb99d882e40ab") or die('connection failed');
+    return $db;
+}
+getdb();
 
 ?>
