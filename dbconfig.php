b9cbfe96fa1aee6bfcae20128565397b4222b403
supersmitty2018 <48794241+supersmitty2018@users.noreply.github.com>
2019-06-03 09:08:25 +0200

    Connected and queried the database testing8

--- dbconfig.php
+++ dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 $host='ec2-54-221-214-3.compute-1.amazonaws.com';
-$db = 'd9f424d1vt6cmt22';
+$db = 'd9f424d1vt6cmt';
 $username = 'xmmutkkqfftoxs';
 $password = '66872be8b5b4f4677cb34f033b54c18f7b8205e1a133eba23adeb99d882e40ab';
 ?>
